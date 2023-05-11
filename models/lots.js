09b36be4800f229449d61b25c9cd7f35caff2768
Agus Narvaez <agus.narvaez@outlook.com>
2023-05-11 15:50:24 -0300

    Agrego objeto de coordenadas al modelo de lote

--- models/lots.js
+++ models/lots.js
@@ -2,12 +2,12 @@ import mongoose, { Schema } from "mongoose"
 
 
 const lotsSchema = new Schema({
-    number: {type: Number, required: true},
+    number: {type: Number, required: true, unique: true},
     area: {type: Number, required: true},
-    price: {type: Number, required: true},
-    reservationPercentage: {type: Number, required: true},
-    financiation: {type: Boolean, required: true},
-    coordinates: {type: Object, required:false},
-    perimeter:{type: Object, required:false}
+    price: {type: Number, required: true,min:100},
+    reservationPercentage: {type: Number, required: true, min:0,max:100},
+    financiation: {type: Boolean, required: true, default: false},
+    coordinates: {lat:Number, lng: Number},
+    perimeter:{x1:Number,x2:Number,y1:Number,y2:Number}
 })
 export default mongoose.model('Lot',lotsSchema)
