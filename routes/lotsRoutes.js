e2a5a1b3239f636a9c07a3a7de29692670cd47c7
Agus Narvaez <agus.narvaez@outlook.com>
2023-05-11 15:49:51 -0300

    Implemento express validator en creación y actualización de lote

--- routes/lotsRoutes.js
+++ routes/lotsRoutes.js
@@ -2,15 +2,15 @@ import express from "express"
 const router = express.Router()
 
 import lotsController from "../controllers/lotsController.js"
-
+import {validateCreate,validateUpdate} from "../middlewares/lotsValidations.js"
 
 router.get('/',lotsController.index)
 
 router.get('/:id',lotsController.read)
 
-router.post('/add',lotsController.create)
+router.post('/create',validateCreate,lotsController.create)
 
-router.put('/update/:id',lotsController.update)
+router.put('/update/:id',validateUpdate,lotsController.update)
 
 router.delete("/delete/:id",lotsController.delete)
 
