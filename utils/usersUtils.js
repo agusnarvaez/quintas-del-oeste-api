d8cce68ee1a83e1edf93d0250d8f7cdc4f53ec54
Agus Narvaez <agus.narvaez@outlook.com>
2023-06-11 17:16:49 -0300

    Agrego utils de usuario para peticiones

---
+++ utils/usersUtils.js
@@ -0,0 +1,23 @@
+import  User  from '../models/users.js'
+
+//* Getters
+const getAllUsers = async () => {
+    try {
+      const lots = await User.find().exec()
+      return lots
+    } catch (e) {
+      console.error(e)
+    }
+}
+const getUserById = async (req,res,next) => {
+    try {
+      const lot = await User.findById(req.params.id)
+      return lot
+    } catch (e) {
+      //console.error(e)
+      return false
+    }
+}
+
+
+export {getAllUsers, getUserById}
