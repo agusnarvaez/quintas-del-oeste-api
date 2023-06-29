4ee91dccecaabbd4c2274bed491820f3e00b527a
Agus Narvaez <agus.narvaez@outlook.com>
2023-06-29 18:59:48 -0300

    Corrijo coordenadas y perímetro en el modelo del Lote

--- src/models/lots.model.js
+++ src/models/lots.model.js
@@ -1,15 +1,24 @@
 //* Importo mongoose y el Schema para crear el modelo de datos
 import mongoose, { Schema } from "mongoose"
-
+import User from "./user.model.js"
 //* Schema de los lotes
 const lotSchema = new Schema({
     number: {type: Number, required: true, unique: true},
     area: {type: Number, required: true},
-    price: {type: Number, required: true,min:100},
+    price: {type: Number, required: true,min:10000},
     reservationPercentage: {type: Number, required: true, min:0,max:100},
     financiation: {type: Boolean, required: true, default: false},
-    coordinates: {lat:{type:Number, default:0}, lng: {type:Number, default:0}},
-    perimeter:{x1:{type:Number, default:0},x2:{type:Number, default:0},y1:{type:Number, default:0},y2:{type:Number, default:0}}
+    coordinates: {
+        lat: {type: Number, default:0},
+        lng: {type: Number, default:0}
+    },
+    perimeter:{
+        x1:{lat: {type: Number, default:0}, lng: {type: Number, default:0}},
+        x2:{lat: {type: Number, default:0}, lng: {type: Number, default:0}},
+        y1:{lat: {type: Number, default:0}, lng: {type: Number, default:0}},
+        y2:{lat: {type: Number, default:0}, lng: {type: Number, default:0}}
+    },
+    reservationUser: {type: String, default: null}
 },{
     timestamps: true //* Crea la fecha de creación y actualización
 })
