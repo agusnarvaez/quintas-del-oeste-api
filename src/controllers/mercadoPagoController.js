9997f56fd06171c8d88c315f63b8d5038c9af9f8
Agus Narvaez <agus.narvaez@outlook.com>
2023-07-13 03:35:26 -0300

    Agrego ejecución de feedback de pago

--- src/controllers/mercadoPagoController.js
+++ src/controllers/mercadoPagoController.js
@@ -1,10 +1,26 @@
 //* Importo mp para procesar el pago
 import mp from "../utils/mercadoPago.js"
 import cache from 'memory-cache'
+import {createAccessToken} from '../utils/jwt.js'
 const controller = {
-    feedback: (req, res) => {
-        //* Envía la respuesta de mercado pago a la vista
-        res.send(req.query)
+    feedback: async (req, res) => {
+        try{
+            //* Envía la respuesta de mercado pago a la vista
+            const response = {
+                payment: {
+                    status:req.payment_status,
+                    payment_id:req.payment_id,
+                },
+                reservation: req.reservationData
+            }
+
+            //* Devuelve los datos de la respuesta
+            return res.status(200).json({ response })
+
+        }catch(error){
+            console.log(error)
+            res.status(500).json({error})
+        }
     },
     proccess: async (req, res) => {
         try{
@@ -28,16 +44,21 @@ const controller = {
     createOrder: async (req, res) => {
         try{
             const reservation = req.body
-            cache.put('reservation', reservation)
+            await cache.put('reservation', reservation, 300000)
+
+
 
             const result = await mp([
                 {
                     title: `Reserva de lote ${reservation.lot.number} Manzana ${reservation.lot.block}`,
-                    unit_price: Number(reservation.lot.reservationPrice),
+                    unit_price: 10000,
                     currency_id: 'ARS',
                     quantity: 1,
                 }
             ])
+            //* Crea el token del ID de las preferencias y lo guarda en una cookie
+            const preferenceIdToken = await createAccessToken({preference_id:result.response.id})
+            res.cookie('preferenceIdToken',preferenceIdToken)
 
             const init_point = result.response.init_point
             res.status(200).json({status:"Pago Creado",/* id:result, */ initPoint: init_point})
